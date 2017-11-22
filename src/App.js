0aac17892c2f3b2e35ff23f730470f1a6568359d
Luiz <luiz.carlos.limajf@gmail.com>
2017-11-22 21:11:35 -0200

    Configuração do react-bootsrap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Navbar, Grid, Row, Col} from 'react-bootstrap';
+import ProductList from './components/ProductList';
+import ShoppingCart from './components/ShoppingCart';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div>
+      <Navbar inverse staticTop>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#">Estudo React-Redux</a>
+          </Navbar.Brand>
+        </Navbar.Header>
+      </Navbar>
+      <Grid>
+        <Row>
+          <Col sm={8}>
+            <ProductList />
+          </Col>
+          <Col sm={4}>
+            <ShoppingCart />
+          </Col>
+        </Row>
+      </Grid>
+    </div>
     );
   }
 }
